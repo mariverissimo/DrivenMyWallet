1a0fa54217ed9e2804b2f20de1dfda50d9d98205
Marianna Vessimo <vessimohgmarianna@gmail.com>
2025-05-01 14:42:23 -0300

    final changes

--- database/database.js
+++ database/database.js
@@ -2,7 +2,19 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const client = new MongoClient(process.env.DATABASE_URL);
+const DB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB Atlas Connected!'))
+.catch(err => console.error('Error connecting to MongoDB Atlas:', err));
+
 
 
 async function connectDB() {
