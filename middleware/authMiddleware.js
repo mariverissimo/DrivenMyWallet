3ededcc280140e451089b00fa1348d053dc03ae7
Marianna Vessimo <vessimohgmarianna@gmail.com>
2025-05-01 11:39:57 -0300

    Token authentification added

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+export async function authenticateToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader?.startsWith("Bearer ")) {
+    return res.status(401).send({ message: "Token não enviado." });
+  }
+
+  const token = authHeader.replace("Bearer ", "");
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId; // salva ID do usuário para uso posterior
+    next();
+  } catch (err) {
+    return res.status(401).send({ message: "Token inválido." });
+  }
+}
